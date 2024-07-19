bf6e0a3429c6b161d2963a767c5dcc713b4a2c48
juansierrah <jusierrah@unal.edu.co>
2024-07-19 10:12:00 -0500

    update

--- objetos.js
+++ objetos.js
@@ -1,8 +1,38 @@
-let objpersonas{
-    nombre = "andres"
-    apellido = "hernadez"
-    email = "jusierra"
+
+let telefonos = {
+    tel_1: "4540157",
+    tel_2: "2764578",
+}
+console.log("numeros_tel", telefonos);
+
+let objpersona_1  = {
+    nombre: "andres",
+    apellido: "hernadez",
+    email: "anhernadez",
+    telefono: telefonos,
 
 }
-console.table(objpersonas);
-console.log(objpersonas.nombre)
+
+let objpersona_2  = {
+    nombre: "maria",
+    apellido: "yepes",
+    email: "mpyepes",
+    telefono: telefonos,
+
+}
+
+let objpersona_3  = {
+    nombre: "juan",
+    apellido: "sierra",
+    email: "jusierra",
+    telefono: telefonos,
+
+}
+
+let personas = {
+    pers_1: objpersona_1,
+    pers_2: objpersona_2,
+    pers_3: objpersona_3,
+    
+}
+console.log("personas", personas);
